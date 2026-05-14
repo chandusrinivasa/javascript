46a861c3694fcdc8778bebb9ccda80ea6655de75
chandusrinivasa <chandushree70@gmail.com>
2026-05-14 16:23:22 +0000

    arrays methods

--- .devcontainer/array.js
+++ .devcontainer/array.js
@@ -41,6 +41,7 @@ let result = fruits.slice(1, 3); // in thses the 1 is start and 3 end where it i
 
 console.log("slice done array",result);
 console.log("original array without silce operatuon",fruits);
+
 // splice = where in this it will make the changes in the original array
 // , the changes made array will be the final array
 // changes can be => add, remove update 
@@ -50,8 +51,95 @@ let final = cars.splice(1,3)//in this the stsrt and the end is included .
 // then in this we are removing the element form 1 to 3 in the array
 console.log("with splice operation",final)// in this it will show which elemnt are you removing
 console.log("without splice operation",cars)//  [ 'BMW', 'Defender' ] after removing element from the array resulted array is this
+// add
+let resultss = cars.splice(1,0,"chandu loves chandhan")
+console.log("aaaaaaaaaa",cars)
+// in this we used the spread operator to merge the two array
+adding_two_arrays_into_one_array = [...cars,...fruits]
+console.log(adding_two_arrays_into_one_array)
+
+
+//The spread operator in JavaScript is written as: ...
+// It is used to spread or expand elements from:
+// arrays
+// objects
+// strings
+// it used to sperad the things in it, meanigs separiting
+// in the above program is used to merge the array
+//1.Spread Operator with Arrays
+
+let fruitss = ["apple", "banana", "mango"];
+
+let newFruits = [...fruitss];
+
+console.log(newFruits);
+// 2. Add elements
+let numbers = [2, 3, 4];
+
+let resultesss = [1, ...numbers, 5];
+
+console.log(result);
+
+//Spread with Strings
+let name = "Chandu";
+
+let letters = [...name];
+
+console.log(letters);// output ["C", "h", "a", "n", "d", "u"]
+
+//Update Object Values
+let user = {
+  name: "Ram",
+  age: 20
+};
+
+let updatedUser = {
+  ...user,
+  age: 25
+};
+
+console.log(updatedUser);// output 
+//{
+//   name: "Ram",
+//   age: 25
+// }
 
 
 
+//FLAT
+// it is used to remove the nested array or flatend the array
+let arr = [1, [2, [3, [4]]]];
+
+console.log(arr.flat(Infinity));
+//inside the flat means as ap parametr you can send infinity => removes all the nested array
+// 1=> remove only one nested array
+// 2 => remove two nested array like this it goes on
 
 
+console.log(Array.isArray([2,1,3,4,5]))
+// is array is used to check the given thing is array is not
+console.log(Array.isArray(['hello']))
+
+// form => is used to make the array means convert the given thing in array
+console.log(Array.from("chandhan"))// output 
+console.log(Array.from({name:"chandu"}))// output [] because,
+//Array.from() looks for two things inside the object:
+
+// Iterable data (like string, array, set)
+// length property (array-like object)
+
+
+
+// of
+// Array.of()
+// Array.of() creates a new array from the values you give.
+
+// Syntax
+// Array.of(value1, value2, value3)
+
+console.log("example for array.of")
+val1 = 10, val2=20,val3=30
+console.log(Array.of(val1,val2,val3))
+
+console.log("example of 2")
+console.log(Array.of(1, 2, 3));
